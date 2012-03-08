5eaf9237bb6629088e14e8217332d402775c2738
Ben Hartman <bbhartman@alaska.edu>
2012-03-07 23:59:31 -0900

    Adding the beginning of the jump logic

--- board.py
+++ board.py
@@ -14,7 +14,6 @@ class board():
 		"""Creates a starter board"""
 		self.location = list("bbbbbbbbbbbb" + "eeeeeeee" + "rrrrrrrrrrrr")
 
-
 	def randomBoard(self): #Generates a random board, NOTE: Will not be a valid board, just one with random stuff on it
 		"""Generates a random board
 		NOTE: The generated board may not be an actually valid board (one that could be actually played to)
@@ -23,3 +22,19 @@ class board():
 		choices = ['e', 'r', 'b', 'R', 'B']
 		for x in xrange(32):
 			self.location.append(random.choice(choices))
+
+	def jumpsPresent(self,color):
+		"""Returns the empty set if no jumps, else a list of pieces that can jump"""
+		for square,location in zip(self.location,range(32)):
+			if spot is not ('e' or 'r') and location <= 24: # So we can go down-left and down-right
+				if (location % 4 is not 0) and (self.location[location+7] is 'e'): #Down-left jump
+					if location % 8 < 4: #Odd numbered row
+						if color = "red"
+						otherPiece = 
+						if 
+
+
+
+	def genMoves(self,color):
+		"""Generator that returns possible moves based off of our current configuration"""
+		jumpPieces = jumpsPresent(color)

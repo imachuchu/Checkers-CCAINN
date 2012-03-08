b69486ed6bff0c240e2713bbd35685e76ed88384
Ben Hartman <bbhartman@alaska.edu>
2012-03-07 21:09:22 -0900

    Added a correct default constructor for the board

--- board.py
+++ board.py
@@ -7,8 +7,18 @@ import itertools #For sweet fast list functions
 import math #For the e constant
 
 class board():
+	""" Represents one checker board
+	"""
+
+	def __init__(self):
+		"""Creates a starter board"""
+		self.location = list("bbbbbbbbbbbb" + "eeeeeeee" + "rrrrrrrrrrrr")
+
 
 	def randomBoard(self): #Generates a random board, NOTE: Will not be a valid board, just one with random stuff on it
+		"""Generates a random board
+		NOTE: The generated board may not be an actually valid board (one that could be actually played to)
+		"""
 		self.location = []
 		choices = ['e', 'r', 'b', 'R', 'B']
 		for x in xrange(32):

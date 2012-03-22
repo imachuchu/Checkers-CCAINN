666288e8a7c6e94e1b08cd3b41235a5b641c58d3
Ben Hartman <bbhartman@alaska.edu>
2012-03-21 22:13:59 -0800

    Completely changed how the move generator works

--- board.py
+++ board.py
@@ -5,6 +5,7 @@ Ben Hartman 3/4/2012
 import random #For random generation functions
 import itertools #For sweet fast list functions
 import math #For the e constant
+import copy
 
 class board():
 	""" Represents one checker board
@@ -17,49 +18,86 @@ class board():
 		"""
 		self.location = list("bbbbbbbbbbbb" + "eeeeeeee" + "rrrrrrrrrrrr")
 
-	def randomBoard(self): #Generates a random board, NOTE: Will not be a valid board, just one with random stuff on it
+	def randomBoard(self):
 		"""Generates a random board
 		NOTE: The generated board may not be an actually valid board (one that could be actually played to)
 		"""
 		choices = ['e', 'r', 'b', 'R', 'B']
 		self.location = [random.choice(choices) for x in range(32)]
 
-	def jumpsPresent(self,color): # !NOTE: Only works for black currently!
-		"""Returns the empty set if no jumps, else a set of pieces that can jump"""
-		jumpList = set({})
+	def genMoves(self,color,jumpFound=False): # !NOTE: Only works for black currently!
+		"""Returns a set of future boards generatable from the current board. If none returns an empty set"""
+		jumpBoards = set({})
 		for square,location in zip(self.location,range(32)):
 			if spot is not ('e' or 'r') and location <= 24: # So we can go down-left and down-right
 				if (location % 4 is not 0) and (self.location[location+7] is 'e'): #Down-left jump
 					if location % 8 < 4: #Odd numbered row
 						if self.location[location+4] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 					else:
 						if self.location[location+3] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jum[Boards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 				if (location % 4 is not 3) and (self.location[location+9] is 'e'): #Down-right jump
 					if location % 8 < 4: #Odd numbered row
 						if self.location[location+5] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 						if self.location[location+4] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
+
 			if spot is not ('e' or 'b') and location >= 9: # So we can go up-left and up-right
 				if (location % 4 is not 0) and (self.location[location-7] is 'e'): #Up-left jump
 					if location % 8 < 4: #Odd numbered row
 						if self.location[location-4] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 					else:
 						if self.location[location-5] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 				if (location % 4 is not 3) and (self.location[location-9] is 'e'): #Up-right jump
 					if location % 8 < 4: #Odd numbered row
 						if self.location[location-3] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
 						if self.location[location-4] is ('r' or 'R'):
-							jumpList.add(location)
+							newBoard = copy.copy(self)
+							tempSet = newBoard.genMoves(color, jumpFound=True)
+							if tempSet:
+								jumpBoards = board | tempSet
+							else:
+								jumpBoards.add(newBoard)
+		if jumpBoards or jumpFound: #We've found at least one jump
+			return jumpBoards
 
-
-
-
-	def genMoves(self,color):
-		"""Generator that returns possible moves based off of our current configuration"""
-		jumpPieces = jumpsPresent(color)
